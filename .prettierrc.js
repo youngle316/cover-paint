2fd3f5f50e098ba953dc1067117c27d94fd59b72
杨乐乐 <youngle316@gmail.com>
2023-09-20 16:53:25 +0800

    chore: add prettier eslint and lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+};
