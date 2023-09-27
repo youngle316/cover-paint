2615e90e30576cec4dad139aaabaf60adec5c5f3
Xat <i@xatloon.me>
2023-09-27 16:08:28 +0000

    chore: adjust menu margin

--- app/components/ThemeSelector.tsx
+++ app/components/ThemeSelector.tsx
@@ -31,7 +31,7 @@ function ThemeSelector() {
         <SunMoon className="h-5 w-5 md:h-7 md:w-7" />
       </button>
       <div
-        className="absolute -right-2 z-10 -mt-1 hidden w-32 rounded-lg border border-gray-100
+        className="absolute -right-2 z-10 mt-1 hidden w-32 rounded-lg border border-gray-100
       bg-white text-left text-sm shadow-lg group-hover:block dark:bg-black"
       >
         <div className="p-1">
